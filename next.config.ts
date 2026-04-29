d78ac2df81a74b700b4ef6cb8c4a6d866571ecd1
Gabriel Nascimento <gabriel.nascimenton.19@gmail.com>
2026-04-29 14:59:29 -0300

    change lint

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,6 @@ import path from "node:path";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias ?? {}),
-      "@": path.resolve(process.cwd(), "src"),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
