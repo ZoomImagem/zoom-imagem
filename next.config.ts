548ddfc9136f7ba28f00c8797c5de425a25a04e1
Keverton Reis <kevertondrv@gmail.com>
2026-05-03 17:04:58 -0300

    ajust card avaliations width mobile

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  allowedDevOrigins: ["192.168.0.135"],
+
   reactCompiler: true,
   images: {
     remotePatterns: [
