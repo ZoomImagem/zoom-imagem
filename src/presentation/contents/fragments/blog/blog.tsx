8b540bd2943d4c0ae992939ed85d833f5802e4c7
Keverton Reis <kdrvdeveloper@gmail.com>
2026-05-24 14:53:24 -0300

    insert blog tag

---
+++ src/presentation/contents/fragments/blog/blog.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { ButtonTag } from "@/components";
+import { FiArrowRight } from "react-icons/fi";
+import styles from "./blog.module.scss";
+
+const BlogComponent = () => {
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default BlogComponent;
